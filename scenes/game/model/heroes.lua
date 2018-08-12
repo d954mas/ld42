c63523d6649d80902dda663278f5363ad12eb1ae
Nikita.Tikhomirov <nikita.tikhomirov@nevosoft.ru>
2018-08-13 02:07:49 +0300

    test

--- scenes/game/model/heroes.lua
+++ scenes/game/model/heroes.lua
@@ -3,22 +3,30 @@ local LUME = require "libs.lume"
 local M = {}
 ---@class Hero
 local BaseModel = CLASS.class("BaseModel")
-function BaseModel:initialize(productivity_mul)
-    self.max_productivity = 1
-    self.max_happy = 1
-    self.productivity = 0.5
-    self.happy = 1
+function BaseModel:initialize(mp, mh, pr, h, im, ip, tm, tp, imp, nh, np, need)
+    self.max_productivity = mp
+    self.max_happy = mh
+    self.productivity = pr
+    self.happy = h
+    self.irr_minus = im
+    self.irr_plus = ip
+    self.temp_minus = tm
+    self.temp_plus = tp
+    self.importance = imp
+    self.norm_happy = nh
+    self.norm_productivity = np
+    self.need_space = need
 end
 
 function BaseModel:reset()
     self.productivity = 0.5
-    self.happy = 1
+    self.happy = 0.7
 end    
 
 ---@param number space_percent процент от диска
 function BaseModel:act(space_percent)
     pprint(space_percent)
-    if space_percent >= 0.2499 then
+    if space_percent > self.need_space then
         self.happy = self.happy + 0.1
     else
         self.happy = self.happy - 0.1
@@ -39,9 +47,9 @@ function BaseModel:act(space_percent)
 end
 
 
-M.hero_1 = BaseModel(1,2,3,4,5)
-M.hero_2 = BaseModel(1,2,3,4,5)
-M.hero_3 = BaseModel(1,2,3,4,5)
+M.hero_1 = BaseModel(1,1,0.5,0.6,1,2,1,1.5,1,0.5,0.5,0,3)
+M.hero_2 = BaseModel(1,1,0.1,0.5,5)
+M.hero_3 = BaseModel(1,1,3,4,5)
 
 return M
 
